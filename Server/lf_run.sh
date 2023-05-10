613272faa90c80d33f0eb0ff64291c4239738937
MrWaterhouse <thiswaterhouse@gmail.com>
2023-05-10 17:04:38 -0400

    v1.3.0 - Updated paths and changed versioning to match numbering of other scripts.

--- Server/lf_run.sh
+++ Server/lf_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#LeakyFaucet Server RUN v3.1
+#LeakyFaucet Server RUN v1.3.0
 #Written: Mr. Waterhouse 
-#May 7, 2023
+#May 10, 2023
 #
 #This is script 4 of 4 required on the server side of LeakyFaucet.
 #
@@ -15,16 +15,16 @@ sudo systemctl stop lf_listener
 sudo systemctl stop lf_filemon
 
 # 3. Remove /home/ubuntu/pico_data.txt
-rm /home/ubuntu/lf_data.txt
+rm /home/ubuntu/lfdata/lf_data.txt
 
 # 4. Create an empty /home/ubuntu/lf_data.txt file
-touch /home/ubuntu/lf_data.txt
+touch /home/ubuntu/lfdata/lf_data.txt
 
 # 5. Clear nohup data since some data may end up in there
 rm /home/ubuntu/nohup.out
 
 # 6. Clear out the exfil log folder. (Do not delete files younger than 10 minutes in case it is still in active verification.)
-find /home/ubuntu/lf_exfil/* -mmin +10 -type f -print0 | xargs -0 rm -f
+find /home/ubuntu/lfdata/lf_exfil/* -mmin +10 -type f -print0 | xargs -0 rm -f
 
 # 7. Start Listener in the background and suppress console
 sudo systemctl start lf_listener
